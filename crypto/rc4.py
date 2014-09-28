f0b5c9534e5589c761dc6c24ba45b139f28183f8
Andrew Rosen <rosen@cs.gsu.edu>
2014-09-28 18:33:57 -0400

    Added nice output, and removed extraneous code that never worked.

--- crypto/rc4.py
+++ crypto/rc4.py
@@ -1,50 +1,6 @@
 from crypto_tools import phi, psi, phi2, getFreqs
 import sys
 
-#!/usr/bin/env python
-#
-#       RC4, ARC4, ARCFOUR algorithm
-#
-#       Copyright (c) 2009 joonis new media
-#       Author: Thimo Kraemer <thimo.kraemer@joonis.de>
-#
-#       This program is free software; you can redistribute it and/or modify
-#       it under the terms of the GNU General Public License as published by
-#       the Free Software Foundation; either version 2 of the License, or
-#       (at your option) any later version.
-#       
-#       This program is distributed in the hope that it will be useful,
-#       but WITHOUT ANY WARRANTY; without even the implied warranty of
-#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#       GNU General Public License for more details.
-#       
-#       You should have received a copy of the GNU General Public License
-#       along with this program; if not, write to the Free Software
-#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#       MA 02110-1301, USA.
-#
-
-
-def rc4crypt(data, key):
-    x = 0
-    box = range(256)
-    for i in range(256):
-        x = (x + box[i] + ord(key[i % len(key)])) % 256
-        box[i], box[x] = box[x], box[i]
-    x = 0
-    y = 0
-    out = []
-    for char in data:
-        x = (x + 1) % 256
-        y = (y + box[x]) % 256
-        box[x], box[y] = box[y], box[x]
-        out.append(chr(ord(char) ^ box[(box[x] + box[y]) % 256]))
-    
-    return ''.join(out)
-
-
-
-
 
 class RC4(object):
     def __init__(self, seed):
@@ -103,10 +59,10 @@ class RC4(object):
 
 def main():
     words  = open("words", 'r')
-    corpus = list(open("cipher2", 'r').read())
+    corpus = list(open("cipher4", 'r').read())
     #print list(corpus)
-    best_word = 'NO'
-    best_plain = 'NO'
+    best_word = 'GARBAGE'
+    best_plain = 'GIBBERISH'
     best_phi  = -50
    
     i  = 0 
@@ -115,7 +71,7 @@ def main():
         i +=1
         word = word.strip()
         if i % 10000 ==0 :
-            print i
+            print i, "trials."
         #print word
         while len(word) < 8:
             word =  word + '\x00'
@@ -129,18 +85,9 @@ def main():
             best_word = word
             best_plain = output
             print best_word, best_phi
-
-        """
-        good =  True
-        for char in trial:
-            if ord(char)> 128:
-                good = False 
+            if score > 0.025:
                 break
-        if good:
-            print word
-            print trial
-            break
-            """
+
     print best_word, best_phi, best_plain 
     
 
