9174c8f2c01a7a52b88b5ac0b6886ba0ee2b27f3
Andrew Rosen <rosen@cs.gsu.edu>
2014-11-25 15:39:38 -0500

    Experiment 2 done, except for last bit

--- project/dhtPotato.py
+++ project/dhtPotato.py
@@ -2,8 +2,36 @@ import random
 import time
 from hashlib import sha1
 random.seed(12345)
-print "The keysize in bits is", sha1().digest_size * 8
+print "Experiment Parameter 1: The keysize in bits is", sha1().digest_size * 8
 
+
+def getHash(value):
+    return long(sha1(value).hexdigest(),16)
+
+def hashBetween(target, left, right):
+    if left ==  right:
+        return True
+    if target == left or target == right:
+        return False
+    #print target, "<", right, "and", target, ">", left, target < right and target > left
+    if target < right and target > left:
+        return True
+    #print left, ">", right, left > right 
+    if left > right :
+        #print left, ">", target, "and", target, "<", right, left > target and target < right
+        if left > target and target < right:
+            return True
+        #print left, "<", target, "and", target, ">", right, left < target and target > right
+        if left < target and target > right:
+            return True
+    return False
+
+
+def generateRandomIP():
+    return "".join(map(str, (random.randint(0,255) for _ in range(4))))
+
+def generateRandomPort():
+    return str(random.randint(0,65535))
 
 
 
@@ -53,51 +81,46 @@ def testSuccessors(trials):
                 break
         times.append(time.time() - start)
         attempts.append(tries)
-    print "Successes:", success/trials,"Avg time:", sum(times)/trials, sum(attempts)/trials
+    print "Network Size:", trials, "Successes:", success/trials,"Avg time:", sum(times)/trials, "Avg Ports tried:", sum(attempts)/trials
 
 def testCollisions(networkSize):
     mal = generateRandomIP()
-    victims =  [getHash(generateRandomIP()+generateRandomPort()) for _ in xrange(trials)]
+    victims =  [getHash(generateRandomIP()+generateRandomPort()) for _ in xrange(networkSize)]
     victims = sorted(victims)
     success = 0.0
-    malKeys = [getHash(generateRandomIP()+str(p)) for p in xrange(49152,65535)]
+    injections = 0.0
+    mal = generateRandomIP()
+    malKeys = [getHash(mal+str(p)) for p in xrange(49152,65535)]
     malKeys = sorted(malKeys)
-    for i in xrange(trials -1):
-            pass
+
+    """
+    Let's make this an O(n) operation
+    """
+    malIndex = 0
+    for i in xrange(networkSize - 1):
+        a = victims[i]
+        b = victims[i+1]
+        injected = False
+        while   malIndex < len(malKeys) and malKeys[malIndex] < b:
+            attempt = malKeys[malIndex]
+            if hashBetween(attempt,a,b):
+                injected  = True
+                injections += 1
+            malIndex +=1
+        if injected:
+            success += 1
+    print "Network Size:", networkSize, "Regions Injected :", success/networkSize, "avg Injections per region", injections/networkSize
+
+        
   
 
-def getHash(value):
-    return long(sha1(value).hexdigest(),16)
 
-def hashBetween(target, left, right):
-    if left ==  right:
-        return True
-    if target == left or target == right:
-        return False
-    #print target, "<", right, "and", target, ">", left, target < right and target > left
-    if target < right and target > left:
-        return True
-    #print left, ">", right, left > right 
-    if left > right :
-        #print left, ">", target, "and", target, "<", right, left > target and target < right
-        if left > target and target < right:
-            return True
-        #print left, "<", target, "and", target, ">", right, left < target and target > right
-        if left < target and target > right:
-            return True
-    return False
-
-
-def generateRandomIP():
-    return "".join(map(str, (random.randint(0,255) for _ in range(4))))
-
-def generateRandomPort():
-    return str(random.randint(0,65535))
 
 
 #testWide(100000)
-testSuccessors(100)
-testSuccessors(200)
-testSuccessors(500)
-testSuccessors(1000)
+#testSuccessors(100)
+#testSuccessors(200)
+testCollisions(50000)
+#print "Network Size:", trials, "Successes:", success/trialstestSuccessors(1000)
+"""Linear scale of injections per region"""
 #testSuccessors(20000)
